db1508d769c23e6ced58856f6b3e7488ee0eb539
Nuthanmani <88588757+Nuthanmani@users.noreply.github.com>
2021-10-18 11:33:38 +0530

    Create binarysystem.c

---
+++ binarysystem.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+int binary(int bin1, int bin2)
+{
+    int i = 0, rem = 0, a[20];
+    int prod = 0;
+ 
+    while (bin1 != 0 || bin2 != 0)
+    {
+        a[i++] =(bin1 % 10 + bin2 % 10 + rem) % 2;
+        rem =(bin1 % 10 + bin2 % 10 + rem) / 2;
+        bin1 = bin1 / 10;
+        bin2 = bin2 / 10;
+    }
+    if (rem != 0)
+        a[i++] = rem;
+    --i;
+    while (i >= 0)
+        prod = prod * 10 + a[i--];
+    return prod;
+}
+ 
+int main()
+{
+ 
+    long bin1, bin2,multiply=0;
+   
+    int i = 0, rem = 0, a[20], digit, factor = 1;
+    while(1){
+    printf("Enter the Choice :\n");
+    printf("1.Addition\n2.Multiplication\n3.Exit\n");
+    int n;
+      scanf("%d",&n);
+      if(n==1){
+           printf("Enter the first binary number: ");
+    scanf("%ld", &bin1);
+    printf("Enter the second binary number: ");
+    scanf("%ld", &bin2);
+    while (bin1 != 0 || bin2 != 0)
+    {
+        a[i++] =(bin1 % 10 + bin2 % 10 + rem) % 2;
+        rem =(bin1 % 10 + bin2 % 10 + rem) / 2;
+        bin1 = bin1 / 10;
+        bin2 = bin2 / 10;
+    }
+    if (rem != 0)
+        a[i++] = rem;
+    --i;
+    printf("Sum of two binary numbers: ");
+    while (i >= 0)
+        printf("%d", a[i--]);
+
+      printf("\n");}
+
+      else if(n==2){
+              printf("Enter the first binary number: ");
+    scanf("%ld", &bin1);
+    printf("Enter the second binary number: ");
+    scanf("%ld", &bin2);
+    while (bin2 != 0)
+    {
+        digit =  bin2 % 10;
+        if (digit == 1)
+        {
+            bin1 = bin1 * factor;
+            multiply = binary(bin1, multiply);
+        }
+        else
+            bin1 = bin1 * factor;
+        bin2 = bin2 / 10;
+        factor = 10;
+    }
+    printf("Product of two binary numbers: %ld", multiply);
+    printf("\n");
+
+      }
+      else if(n==3){
+          break;
+      }
+    }
+    return 0;
+}
